8dbc65a73e2e92dd3c83470fbe3c33360ad3b01c
Domeniqque Dylluar <domeniqque@hotmail.com>
2017-09-14 14:45:03 -0400

    change: add appontment success and doctor views

---
+++ resources/views/doctor.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+
+@section('main')
+    <section id="about" class="about-section"><!-- about start -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <h1>Nome do Doutor</h1>
+                    <h1>Especialidade</h1>
+                    <p class="lead">Descrição</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- about close -->
+@endsection
